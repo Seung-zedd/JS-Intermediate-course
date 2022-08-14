0c8dc2dcb9e4ac0fa48beb37683aace590fd88bf
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2022-08-14 11:28:10 +0900

    Update call,apply,bind.js

--- call,apply,bind.js
+++ call,apply,bind.js
@@ -13,9 +13,9 @@ function showThisName() {
   console.log(this.name);
 }
 
-showThisName();
-showThisName.call(mike);
-showThisName.call(tom);
+showThisName();//undefined
+showThisName.call(mike);//"Mike"
+showThisName.call(tom);//"Tom"
 
 function update(birthyear, occupation) {
   this.birthyear = birthyear;
@@ -23,14 +23,14 @@ function update(birthyear, occupation) {
 }
 
 update.call(mike, 1999, "singer");
-console.log(mike);
+console.log(mike);//{name: "Mike", birthyear: 1999, occupation: "singer"}
 
 update.call(tom, 1988, "developer");
-console.log(tom);
+console.log(tom);//{name: "Tom", birthyear: 1988, occupation: "developer"}
 
 /**************************************
  * 위의 update함수를 apply로 바꿔본다면?
- *Math method를 통한 call과 apply의 차이점
+ *Math method를 통한 call과 apply의 차이점:apply는 매개변수를 리터럴 Array 또는 객체를 받을 수 있다.
  **************************************/
 const mike = {
   name: "Mike",
@@ -72,13 +72,13 @@ const user = {
   },
 };
 
-user.showName();
+user.showName();//hello, Mike; 메소드앞은 this를 가르키는데 this -> user가 됨.
 
-let fn = user.showName; //fn에 할당하면 this를 잃어버림
+let fn = user.showName; //변수 fn에 할당하면 this를 잃어버림
 
 fn.call(user);
 fn.apply(user); //apply는 객체를 받을 수도 있다.
 
 let boundfn = fn.bind(user); //어떤 변수에 할당하면 this를 잃어버리는데, bind는 this값을 바꿔줌.
 
-boundfn();
+boundfn();//hello, Mike
